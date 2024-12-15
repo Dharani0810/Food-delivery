86dc10d18bd88c69e12564c7c3bed2be4f088d15
Dharani S <131860883+Dharani0810@users.noreply.github.com>
2024-12-16 01:11:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
 import dotenv from 'dotenv'
 import cartRouter from "./routes/cartRoute.js"
-import orderRouter from "./routes/OrderRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 dotenv.config();
 
 
